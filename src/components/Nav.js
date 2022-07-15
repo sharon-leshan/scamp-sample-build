7140d878f4fdf0899c90a17181f18c3ca61105b8
sharon-leshan <maleos380@gmail.com>
2022-07-15 10:16:43 +0300

    Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>
+        Untitled UI
+      </li>
+      <li>
+        Home
+      </li>
+      <li>
+        Resources
+      </li>
+      <li>
+        Pricing
+      </li>
+    </ul>
+  )
+}
+
+export default Nav
