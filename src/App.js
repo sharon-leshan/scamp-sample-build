7f00bac1acda23619f67d30424148ed788a5c0e7
sharon-leshan <maleos380@gmail.com>
2022-07-17 18:57:03 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import routes from './routes/routes';
 
 function App() {
   return (
-   <div></div>
+    <>
+    <Header/>
+   {routes()}
+   </>
   );
 }
 
