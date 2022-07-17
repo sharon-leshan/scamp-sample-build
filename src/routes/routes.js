5e8c45df64ce633fc6fe3108a19b7b34d7832108
sharon-leshan <maleos380@gmail.com>
2022-07-17 18:56:44 +0300

    routes.js

---
+++ src/routes/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Resources from '../components/Resources'
+import Pricing from '../components/Pricing'
+import Home from '../components/Home'
+
+const routes = () => {
+  return (
+    <Switch>
+        <Route path='/' exact={true} component={Home} />
+        <Route path='/resources'component={Resources} />
+        <Route path='/pricing' component={Pricing} />
+    </Switch>
+  )
+}
+
+export default routes
