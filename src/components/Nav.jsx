e40fe1c7d8cde616a9f2afd2c269fbbaf59401cf
sharon-leshan <maleos380@gmail.com>
2022-07-19 14:01:38 +0300

    Nav update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import './nav.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import './nav.css';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <ul>     
-      <li>
-        <NavLink to="/" exact={true} activeClassName="active" >Home</NavLink>
-      </li>
-      <li>
-      <NavLink to="/" activeClassName="active" >Resources</NavLink>
-      </li>
-      <li>
-      <NavLink to="/" activeClassName="active" >Pricing</NavLink>
-      </li>
-    </ul>
-  )
-}
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" exact={true} activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/resources" activeClassName="active">
+            Resources
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing" activeClassName="active">
+            Pricing
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
