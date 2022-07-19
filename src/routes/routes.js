19278953f7b7caa45bee1b404868427a410ebd68
sharon-leshan <maleos380@gmail.com>
2022-07-19 14:03:50 +0300

    routes update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Switch, Route} from 'react-router-dom'
-import Resources from '../components/Resources'
-import Pricing from '../components/Pricing'
-import Home from '../components/Home'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Resources from '../components/Resources';
+import Pricing from '../components/Pricing';
+import Home from '../components/Home';
 
 const routes = () => {
   return (
     <Switch>
-        <Route path='/' exact={true} component={Home} />
-        <Route path='/resources'component={Resources} />
-        <Route path='/pricing' component={Pricing} />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/resources" component={Resources} />
+      <Route path="/pricing" component={Pricing} />
     </Switch>
-  )
-}
+  );
+};
 
-export default routes
+export default routes;
