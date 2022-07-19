075acdb7ae9686b3837ded313320edf8c032566c
sharon-leshan <maleos380@gmail.com>
2022-07-19 13:44:27 +0300

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import Nav from './Nav'
-import './header.css'
+import React from 'react';
+import Nav from './Nav';
+import './header.css';
+import { Button } from '@mui/material';
 
 const Header = () => {
   return (
     <header>
-         <div className="left">
-         <div>
-        Untitled UI
+      <div className="left">
+        <div>
+          <img src="" alt="logo" />
+          Untitled UI
+        </div>
+        <Nav />
       </div>
-        <Nav/>
-         </div>
+      <div className="right">
+        <Button variant="text">Log in</Button>
+        <Button variant="contained" id="signup">
+          Sign up
+        </Button>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
